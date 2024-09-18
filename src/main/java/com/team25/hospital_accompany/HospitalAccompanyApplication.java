941c53d52355ba1ad228b6774c0a5514001ab598
wvssm <neoramiregi@gmail.com>
2024-09-18 15:20:28 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/team25/hospital_accompany/HospitalAccompanyApplication.java
@@ -0,0 +1,13 @@
+package com.team25.hospital_accompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAccompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAccompanyApplication.class, args);
+	}
+
+}
